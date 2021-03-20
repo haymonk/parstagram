641685f3fff66c98a9087df39fa530fd967b3ca9
Haymon T <haymonkthit@gmail.com>
2021-03-20 19:52:53 -0400

    Fake comments

--- Parstagram/SceneDelegate.swift
+++ Parstagram/SceneDelegate.swift
@@ -17,16 +17,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-        
-        // Add User persistence across app restarts
+
         if PFUser.current() != nil {
-            let main = UIStoryboard(name: "Main", bundle: nil)
+            login()
+            
+           /* let main = UIStoryboard(name: "Main", bundle: nil)
             let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
             
             window?.rootViewController = feedNavigationController
+            */
         }
+        guard let _ = (scene as? UIWindowScene) else {return}
     }
+    
+    func login() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "FeedNavigationController")
+    }
+    
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
@@ -55,7 +63,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
 
 }
 
